f71ba5ec17c136b044bf6cb87e3d759647d53eb8
Fábio José Dantas <fabiojdantas@ymail.com>
2016-07-19 12:40:31 -0300

    Inclusao de logica de decisao 'if' para o valor zero passado para o calculo fatorial na classe Fatorial

--- Fatorial.java
+++ Fatorial.java
@@ -2,8 +2,10 @@ public class Fatorial {
 	/* metodo iterativo do calculo Fatorial */
 	public int calcularFatorial(int n) {
 		int resultado = 1;
-		for (int i= 2; i <= n; i++) {
-			resultado *= i;
+		if (n > 0) {
+			for (int i= 2; i <= n; i++) {
+				resultado *= i;
+			}
 		}
 		return resultado;		
 	}
