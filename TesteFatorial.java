87ed8fac5a8994386a34333142c1fd2525b1e5c9
Fábio José Dantas <fabiojdantas@ymail.com>
2016-07-27 22:00:17 -0300

    Alteracao do calculo fatorial de 0 para 10 em TesteFatorial.java

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -2,7 +2,7 @@ public class TesteFatorial {
 	//metodo principal do projeto Fatorial
 	public static void main(String [] args) {
 		Fatorial fat = new Fatorial();
-		int resultado = fat.calcularFatorial(0);
+		int resultado = fat.calcularFatorial(10);
 		System.out.println(resultado);
 	}
 }
