4dd092007ac26dede1e03cbaa5c77666c6f0fadf
Fábio José Dantas <fabiojdantas@ymail.com>
2016-07-28 22:23:04 -0300

    Calculo de fatorial de 10 para 5

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -2,7 +2,7 @@ public class TesteFatorial {
 	//metodo principal do projeto Fatorial
 	public static void main(String [] args) {
 		Fatorial fat = new Fatorial();
-		int resultado = fat.calcularFatorial(10);
+		int resultado = fat.calcularFatorial(5);
 		System.out.println(resultado);
 	}
 }
